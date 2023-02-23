3b8d426f9966d48da3e39d3d4642818cfd7e424c
Andrew Hamilton <andrew.w.hamilton@strath.ac.uk>
2023-02-23 21:33:40 +0000

    Add files via upload

---
+++ unity/LineDrawer/Assets/Scripts/LineDrawer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineDrawer : MonoBehaviour
+{
+    private LineRenderer lineRend;
+    private Vector3 mousePos;
+    private Vector3 startMousePos;
+
+    [SerializeField]
+    private Text distanceText;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRend = GetComponent<LineRenderer>();
+        lineRend.positionCount = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            lineRend.SetPosition(0, new Vector3(startMousePos.x, startMousePos.y, 0f));
+            lineRend.SetPosition(1, new Vector3(mousePos.x, mousePos.y, 0f));
+            distance = (mousePos - startMousePos).magnitude;
+            distanceText.text = distance.ToString("F2") + " meters";
+        }
+    }
+}
